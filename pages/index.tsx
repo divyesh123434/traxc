816efb7a441bb463c97c7b3d1fe3f9f6d6790cd3
divyesh123434 <109666160+divyesh123434@users.noreply.github.com>
2022-07-28 16:16:24 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import React from 'react';
+
+const Home = () => {
+  return <div> home </div> 
+};
+
+export default Home;
